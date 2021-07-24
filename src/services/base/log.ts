57c6b7ce7dc9fe55d9dc3c73a8d5e717cef438ef
Sin-Cheng Chen <sin-chengchen@Sin-ChengdeMacBook-Pro.local>
2021-07-24 22:19:54 +0800

    [why] develop [what] make Dockerfile,deployment.yml and modify log path

--- src/services/base/log.ts
+++ src/services/base/log.ts
@@ -3,11 +3,18 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 abstract class MonitorLog {
+  private static _logFolderPath = '../../../logs';
+
   protected _logContent: string;
   private static readonly _logFileName = 'log.txt';
-  private static readonly logFilePath = path.resolve(__dirname, `../../../${MonitorLog._logFileName}`);
+  private static readonly logFilePath = path.resolve(__dirname, `${MonitorLog._logFolderPath}/${MonitorLog._logFileName}`);
 
   writeLogFile(): void {
+    const logFolderPath = path.resolve(__dirname, `${MonitorLog._logFolderPath}`);
+    if (!fs.existsSync(logFolderPath)) {
+      fs.mkdirSync(logFolderPath);
+    }
+    console.info(this._logContent);
     fs.appendFileSync(MonitorLog.logFilePath, this._logContent);
   }
 }
@@ -28,6 +35,6 @@ export abstract class StatusMonitorLog extends MonitorLog {
   protected abstract makeStatusLogContent(blockHeight: number): void;
 
   protected _appendDefaultLog(messages: string[]) {
-    return '\r\n' + messages.join('\r\n');
+    return '\r\n' + messages.join('\r\n') + '\r\n';
   }
 }
