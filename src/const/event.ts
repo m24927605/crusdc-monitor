62be69ebfaba7031d1a421a7415be5aa14e19f1c
Sin-Cheng Chen <sin-chengchen@Sin-ChengdeMacBook-Pro.local>
2021-07-26 20:06:02 +0800

    [why] fix [what] query and filter transaction per block to avoid event listener not firing

--- src/const/event.ts
+++ src/const/event.ts
@@ -1,3 +1,5 @@
+import BigNumber from 'bignumber.js';
+
 export enum WsEventName {
   NewBlock = 'block',
   Error = 'error'
@@ -15,4 +17,36 @@ export enum eventImplementation {
   Redeem = 'event Redeem(address redeemer, uint redeemAmount, uint redeemTokens)',
   Borrow = 'event Borrow(address borrower, uint borrowAmount, uint accountBorrows, uint totalBorrows)',
   RepayBorrow = 'event RepayBorrow(address payer, address borrower, uint repayAmount, uint accountBorrows, uint totalBorrows)'
+}
+
+export class TxEventArg {
+  blockNumber: number;
+  transactionHash: string;
+}
+
+export class MintArg {
+  minter: string;
+  mintAmount: BigNumber;
+  mintTokens: BigNumber;
+}
+
+export class RedeemArg {
+  redeemer: string;
+  redeemAmount: BigNumber;
+  redeemTokens: BigNumber;
+}
+
+export class BorrowArg {
+  borrower: string;
+  borrowAmount: BigNumber;
+  accountBorrows: BigNumber;
+  totalBorrows: BigNumber;
+}
+
+export class RepayBorrowArg {
+  payer: string;
+  borrower: string;
+  repayAmount: BigNumber;
+  accountBorrows: BigNumber;
+  totalBorrows: BigNumber;
 }
--- src/service/monitor.ts
+++ src/service/monitor.ts
@@ -5,8 +5,14 @@ export class Monitor {
   private static _ethSingleton: ETHMonitor;
   private static _crUSDCSingleton: CrUSDCMonitor;
 
-  execute() {
+  async execute() {
     Monitor._ethSingleton = ETHMonitorSingleton.getInstance();
     Monitor._crUSDCSingleton = CrUSDCMonitorSingleton.getInstance();
+
+    await Monitor._ethSingleton.ready();
+    await Monitor._crUSDCSingleton.ready();
+
+    await Monitor._ethSingleton.run();
+    await Monitor._crUSDCSingleton.run();
   }
 }
